817e3a603a019de6265573f8d179cb5c4014c28f
greg@gregPredator <greg19850@gmail.com>
2022-07-11 18:15:20 +0100

    created objects with information about user and AI picks, and game result data

---
+++ script.js
@@ -0,0 +1,13 @@
+const playButton = document.querySelector('.play');
+
+const gameResults = {
+  games: 0,
+  wins: 0,
+  loses: 0,
+  draws: 0,
+}
+
+const gamePicks = {
+  userPick: '',
+  AiPick: '',
+}
