5007090ccc3c309bea238db30ba88a5cf969d222
greg@gregPredator <greg19850@gmail.com>
2022-07-11 18:25:22 +0100

    created pickHand function, to pick user hand

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const playButton = document.querySelector('.play');
+const options = document.querySelectorAll('img');
 
 const gameResults = {
   games: 0,
   wins: 0,
   loses: 0,
   draws: 0,
-}
+};
 
 const gamePicks = {
   userPick: '',
   AiPick: '',
+};
+
+const pickHand = function () {
+  gamePicks.userPick = this.dataset.option;
+
+  options.forEach(option => option.style.boxShadow = '');
+  this.style.boxShadow = '0 0 0 4px red'
 }
+
+options.forEach(option => option.addEventListener('click', pickHand));
