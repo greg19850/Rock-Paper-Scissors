e46782d359ba3dc27d631d0581b7b1796984eebe
greg@gregPredator <greg19850@gmail.com>
2022-07-11 19:43:48 +0100

    created function showResult, showing User and AI selection. Created function gameWinner, checking which option wins the game

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const playButton = document.querySelector('.play');
 const options = document.querySelectorAll('img');
+const showUserSelection = document.querySelector('.leftPanel div:nth-of-type(1) span');
+const showAiSelection = document.querySelector('.leftPanel div:nth-of-type(2) span');
+const showWinner = document.querySelector('.leftPanel h1:nth-last-child(1) span');
 
 const gameResults = {
   games: 0,
@@ -22,6 +25,41 @@ const pickHand = function () {
 
 options.forEach(option => option.addEventListener('click', pickHand));
 
+const gameWinner = () => {
+  if (gamePicks.userPick == 'rock') {
+    if (gamePicks.aiPick == 'paper') {
+      showWinner.textContent = 'AI Wins :('
+    } else if (gamePicks.aiPick == 'scissors') {
+      showWinner.textContent = 'You Win :D'
+    } else {
+      showWinner.textContent = 'It\'s a draw!'
+    }
+  } else if (gamePicks.userPick == 'paper') {
+    if (gamePicks.aiPick == 'scissors') {
+      showWinner.textContent = 'AI Wins :('
+    } else if (gamePicks.aiPick == 'rock') {
+      showWinner.textContent = 'You Win :D'
+    } else {
+      showWinner.textContent = 'It\'s a draw!'
+    }
+  } else if (gamePicks.userPick == 'scissors') {
+    if (gamePicks.aiPick == 'rock') {
+      showWinner.textContent = 'AI Wins :('
+    } else if (gamePicks.aiPick == 'paper') {
+      showWinner.textContent = 'You Win :D'
+    } else {
+      showWinner.textContent = 'It\'s a draw!'
+    }
+  }
+}
+
+const showResult = () => {
+  showUserSelection.textContent = gamePicks.userPick;
+  showAiSelection.textContent = gamePicks.aiPick;
+  gameWinner()
+  options.forEach(option => option.style.boxShadow = '');
+}
+
 const aiSelection = () => {
   const selection = options[Math.floor(Math.random() * 3)].dataset.option
   return selection
@@ -31,7 +69,8 @@ const startGame = () => {
   if (!gamePicks.userPick) return alert('no option selected!!');
 
   gamePicks.aiPick = aiSelection();
-  options.forEach(option => option.style.boxShadow = '');
+
+  showResult()
 }
 
 playButton.addEventListener('click', startGame)
