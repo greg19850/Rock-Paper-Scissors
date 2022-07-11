8b2d8df5852ba6f07c16324a0a357bca4e15e9b2
greg@gregPredator <greg19850@gmail.com>
2022-07-11 20:05:18 +0100

    created function displaying counter

--- script.js
+++ script.js
@@ -25,6 +25,17 @@ const pickHand = function () {
 
 options.forEach(option => option.addEventListener('click', pickHand));
 
+const aiSelection = () => {
+  const selection = options[Math.floor(Math.random() * 3)].dataset.option
+  return selection
+}
+
+const showResult = () => {
+  showUserSelection.textContent = gamePicks.userPick;
+  showAiSelection.textContent = gamePicks.aiPick;
+  options.forEach(option => option.style.boxShadow = '');
+}
+
 const gameWinner = () => {
   if (gamePicks.userPick == 'rock') {
     if (gamePicks.aiPick == 'paper') {
@@ -69,26 +80,24 @@ const gameWinner = () => {
       gameResults.draws++;
     }
   }
+};
+
+const displayCounters = () => {
+  document.querySelector('.rightPanel div:nth-of-type(1) span').textContent = gameResults.games
+  document.querySelector('.rightPanel div:nth-of-type(2) span').textContent = gameResults.wins
+  document.querySelector('.rightPanel div:nth-of-type(3) span').textContent = gameResults.loses
+  document.querySelector('.rightPanel div:nth-of-type(4) span').textContent = gameResults.draws
 }
 
-const showResult = () => {
-  showUserSelection.textContent = gamePicks.userPick;
-  showAiSelection.textContent = gamePicks.aiPick;
-  options.forEach(option => option.style.boxShadow = '');
-}
-
-const aiSelection = () => {
-  const selection = options[Math.floor(Math.random() * 3)].dataset.option
-  return selection
-}
 
 const startGame = () => {
   if (!gamePicks.userPick) return alert('no option selected!!');
 
   gamePicks.aiPick = aiSelection();
-  showResult()
-  gameWinner()
-  gameResults.games++
+  showResult();
+  gameWinner();
+  gameResults.games++;
+  displayCounters()
 }
 
 playButton.addEventListener('click', startGame)
